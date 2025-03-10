4863af0933aad67bd31538b9e64870d720347a74
Alex Kim <jesus2kim@naver.com>
2025-03-09 22:21:45 -0800

    Fixing white page issue when first loading

--- js/background.js
+++ js/background.js
@@ -2,10 +2,16 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpg", "4.jpg", "5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`;
+// imgContainer = document.querySelector(".imgContainer");
+// imgContainer.appendChild(bgImage);
 
-bgImage.src = `img/${chosenImage}`;
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.background=`url(img/${chosenImage}) no-repeat center center fixed`;
+    document.body.style.backgroundSize="cover";
+});
 
-imgContainer = document.querySelector(".imgContainer");
-
-imgContainer.appendChild(bgImage);
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
