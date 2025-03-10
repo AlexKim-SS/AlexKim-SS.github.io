6d7e41af24f8fd5a742ba110e58b2e30a37a24dc
Alex Kim <jesus2kim@naver.com>
2025-03-09 22:35:15 -0800

    Fixing todo View align issue

--- js/background.js
+++ js/background.js
@@ -2,11 +2,6 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpg", "4.jpg", "5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-//const bgImage = document.createElement("img");
-//bgImage.src = `img/${chosenImage}`;
-// imgContainer = document.querySelector(".imgContainer");
-// imgContainer.appendChild(bgImage);
-
 document.addEventListener("DOMContentLoaded", () => {
     document.body.style.background=`url(img/${chosenImage}) no-repeat center center fixed`;
     document.body.style.backgroundSize="cover";
